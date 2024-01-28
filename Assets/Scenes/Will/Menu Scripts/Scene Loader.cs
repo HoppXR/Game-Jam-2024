47e564207834b0c0d4e0a002d7fb639e1e9bfc68
MaxFreya <maxfreya220011@gmail.com>
2024-01-28 15:22:32 -0500

    Menu Update(2)

--- Assets/Scenes/Will/Menu Scripts/Scene Loader.cs
+++ Assets/Scenes/Will/Menu Scripts/Scene Loader.cs
@@ -7,6 +7,36 @@ public class SceneLoader : MonoBehaviour
 {
     public void LoadScene1()
     {
-        SceneManager.LoadScene("Dean", LoadSceneMode.Additive);
+        SceneManager.LoadScene(1);
+    }
+    public void LoadScene2()
+    {
+        SceneManager.LoadScene(2);
+    }
+
+    public void LoadScene3()
+    {
+        SceneManager.LoadScene(3);
+    }
+
+    public void LoadScene4()
+    {
+        SceneManager.LoadScene(4);
+    }
+
+    public void LoadScene5()
+    {
+        SceneManager.LoadScene(5);
+    }
+
+    public void LoadScene6()
+    {
+        SceneManager.LoadScene(6);
+    }
+
+    public void GameExit()
+    {
+        Application.Quit();
+        Debug.Log("Attempted Quit");
     }
 }
